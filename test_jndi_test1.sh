3db4c6e3946618cf8997c563546ffd8e3d6d8cf4
Johan Wärlander <johan@snowflake.nu>
2016-11-07 16:38:21 +0100

    Initial baseline for test of JNDI connection mixup

---
+++ test_jndi_test1.sh
@@ -0,0 +1,5 @@
+set +x
+export KETTLE_HOME=$HOME/jndi_problem/config
+export KETTLE_JNDI_ROOT=/$HOME/jndi_problem/jndi
+cd $HOME/pentaho6/data-integration
+./pan.sh -file $HOME/jndi_problem/test_jndi_test1.ktr
